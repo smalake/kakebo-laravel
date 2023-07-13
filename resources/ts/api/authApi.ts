4157bcc64884bc68f892645f4f111de90f781fa0
smalake <41889150+smalake@users.noreply.github.com>
2023-07-13 22:37:45 +0900

    ログイン機能を実装

--- resources/ts/api/authApi.ts
+++ resources/ts/api/authApi.ts
@@ -2,5 +2,5 @@ import { axiosClient } from "./axiosClient";
 
 export const authApi = {
     register: (params: object) => axiosClient.post("/register", params),
-    login: (params: string) => axiosClient.post("/login", params),
+    login: (params: object) => axiosClient.post("/login", params),
 };
