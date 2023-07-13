b7ba41a21655deebb0e664ce59588257ed438ee4
smalake <41889150+smalake@users.noreply.github.com>
2023-07-13 23:58:28 +0900

    認証時のバリデーションを修正

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,22 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
-
+use App\Http\Requests\LoginRequest;
 
 class LoginController extends Controller
 {
-    public function login(Request $request)
+    public function login(LoginRequest $request)
     {
-        //バリデーション
-        $credentials = $request->validate([
-            'uid' => ['required']
-        ]);
 
         // UIDがDBに登録されているか確認
         $user = User::where('uid', $request->uid)->first();
-        Log::debug('【テスト】uid = ' . $user);
 
         if (empty($user)) {
             // ユーザがDBに登録されていなかった場合
