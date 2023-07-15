793de6911a385e74373993def24c8cf496e1a20e
smalake <41889150+smalake@users.noreply.github.com>
2023-07-15 10:39:28 +0900

    認証用レイアウトを適用

--- resources/ts/app.tsx
+++ resources/ts/app.tsx
@@ -1,21 +1,30 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import ReactDOM from "react-dom";
+import { CookiesProvider } from "react-cookie";
+import { RecoilRoot } from "recoil";
 import { Register } from "./components/pages/Auth/Register";
 import { Login } from "./components/pages/Auth/Login";
 import { EventRegister } from "./components/pages/EventRegister";
+import { AuthLayout } from "./components/layout/AuthLayout";
 
 const App = () => {
     const title: string = "Hello TypeScript React";
     return (
         <div id="main">
+            <RecoilRoot>
+                <CookiesProvider>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<EventRegister />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
+                            <Route path="/" element={<AuthLayout />}>
+                                <Route index element={<Login />} />
+                                <Route path="login" element={<Login />} />
+                                <Route path="register" element={<Register />} />
+                            </Route>
                 </Routes>
             </BrowserRouter>
+                </CookiesProvider>
+            </RecoilRoot>
         </div>
     );
 };
