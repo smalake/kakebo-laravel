8133cf2adfe482b570bfa2996ab3eb4abb5d91e7
smalake <41889150+smalake@users.noreply.github.com>
2023-07-15 20:39:50 +0900

    不要なコード削除

--- resources/ts/components/layout/MenuLayout.tsx
+++ resources/ts/components/layout/MenuLayout.tsx
@@ -1,10 +1,9 @@
 import { Box } from "@mui/material";
-import React, { useEffect } from "react";
+import React from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import { FooterMenu } from "./FooterMenu";
 import { useRecoilState } from "recoil";
 import { checkAtom } from "@/recoil/CheckAtom";
-import { setupApi } from "@/api/setupApi";
 
 export const MenuLayout = () => {
     const navigate = useNavigate();
