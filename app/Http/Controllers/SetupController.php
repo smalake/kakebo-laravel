cfa95867bda5efd9af9edea30b906af2170405cd
smalake <41889150+smalake@users.noreply.github.com>
2023-07-30 11:41:15 +0900

    セットアップ用コントローラ作成

--- app/Http/Controllers/SetupController.php
+++ app/Http/Controllers/SetupController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Group;
 use App\Models\User;
 use Illuminate\Http\Request;
 use \Symfony\Component\HttpFoundation\Response;
 use Error;
+use Illuminate\Support\Facades\DB;
 
 class SetupController extends Controller
 {
@@ -26,6 +28,37 @@ class SetupController extends Controller
         } catch (Error $e) {
             $json = [
                 'message' => 'Failed Get to GroupID',
+                'error' => $e->getMessage()
+            ];
+            return response()->json($json, Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    // グループを作成
+    public function create(Request $request)
+    {
+        try {
+            // UID取得
+            $uid = $request->input('uid');
+            // グループ作成
+            DB::beginTransaction();
+            $group = Group::create([
+                'manage_uid' => $uid,
+            ]);
+            // グループIDをユーザに適用
+            User::where('uid', $uid)->update([
+                'group_id' => $group->id,
+            ]);
+
+            $json = [
+                'data' => $group->id,
+                'message' => 'Create Group success!',
+                'error' => ''
+            ];
+            return response()->json($json, Response::HTTP_OK);
+        } catch (Error $e) {
+            $json = [
+                'message' => 'Failed Create to Group',
                 'error' => $e->getMessage()
             ];
             return response()->json($json, Response::HTTP_INTERNAL_SERVER_ERROR);
