1962631d5af2776488033ac753a32800232fe340
smalake <41889150+smalake@users.noreply.github.com>
2023-08-17 22:17:30 +0900

    新規登録時にグループも一緒に作成するよう修正 #16

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,30 +3,45 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\RegisterRequest;
-use Illuminate\Http\Request;
+use App\Models\Group;
 use \Symfony\Component\HttpFoundation\Response;
 use App\Models\User;
-use Illuminate\Support\Facades\Hash;
+use Error;
+use Illuminate\Support\Facades\DB;
 
 class RegisterController extends Controller
 {
     // ユーザの新規登録
     public function register(RegisterRequest $request)
     {
-        $user = User::create([
-            'uid' => $request->uid,
-            'group_id' => 0,
-            'name' => $request->name,
-            'type' => $request->type,
-        ]);
+        try {
+            DB::beginTransaction();
+            $group = Group::create([
+                'manage_uid' => $request->uid,
+            ]);
+            $user = User::create([
+                'uid' => $request->uid,
+                'group_id' => $group->id,
+                'name' => $request->name,
+                'type' => $request->type,
+            ]);
+            DB::commit();
 
-        //ユーザの作成が完了するとjsonを返す
-        $json = [
-            'data' => $user,
-            'message' => 'User registration success!',
-            'error' => ''
-        ];
-        return response()->json($json, Response::HTTP_OK);
+            //ユーザの作成が完了するとjsonを返す
+            $json = [
+                'data' => $user,
+                'message' => 'User registration success!',
+                'error' => ''
+            ];
+            return response()->json($json, Response::HTTP_OK);
+        } catch (Error $e) {
+            DB::rollBack();
+            $json = [
+                'message' => 'Failed Insert to Event',
+                'error' => $e->getMessage()
+            ];
+            return response()->json($json, Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
     // グループへの参加ユーザの新規登録
